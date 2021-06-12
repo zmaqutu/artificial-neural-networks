1540dcd85ba62017074ab6376add38d1f946799a
Zongo Maqutu <MQTZON001@myuct.ac.za>
2021-06-12 11:38:59 +0200

    added method to create and train perceptrons

--- XOR.py
+++ XOR.py
@@ -13,11 +13,25 @@ class XOR:
 
 		self.nand_target_labels = [1,1,1,0]
 
+		self.or_perceptron = None
+		self.nand_perceptron = None
+		self.and_perceptron = None
+
+	def create_perceptrons(self):
+		self.or_perceptron = Perceptron(2, bias=-1)
+		self.nand_perceptron = Perceptron(2, bias=-1)
+		self.and_perceptron = Perceptron(2,bias=-1)
+		print("Working up to here")
+
+	def train_perceptrons(self):
+		self.or_perceptron.train(self.training_examples,self.or_target_labels,0.2)
+		self.nand_perceptron.train(self.training_examples,self.nand_target_labels,0.2)
+		self.and_perceptron.train(self.training_examples,self.and_target_labels,0.2)
+
 	def start_XOR(self):
 		#create perceptrons set biases to -1 for now
-		or_perceptron = Perceptron(2,bias=-1)
-		nand_perceptron = Perceptron(2,bias=-1)
-		and_perceptron = Perceptron(2,bias=-1)
+		self.create_perceptrons()
+		self.train_perceptrons()
 
 class driverClass:
 	def main():
