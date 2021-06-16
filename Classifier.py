9b912b624268117aefd24769800e1005768c91c1
Zongo Maqutu <MQTZON001@myuct.ac.za>
2021-06-16 12:50:32 +0200

    added classifier file with layout

---
+++ Classifier.py
@@ -0,0 +1,17 @@
+import numpy as np
+import random
+from Perceptron import Perceptron
+
+class Classifier:
+	def __init__(self):
+		print(1)
+
+	def start_classification(self):
+		print(2)
+
+class driverClass:
+	def main():
+		digit_classifier = Classifier()
+		digit_classifier.start_classification()
+	if __name__ == "__main__":
+		main()
